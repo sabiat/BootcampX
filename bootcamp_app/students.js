5115e99f6b1ff87a73f1c60ed8011027d89b0971
Sabia Tahir <sabiatahir@hotmail.com>
2021-02-04 03:38:40 +0000

    Used parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -31,13 +31,15 @@ const pool = new Pool({
 //   })
 // });
 
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts on cohort_id = cohorts.id
 WHERE cohorts.name LIKE $1
 LIMIT $2;
-`)
+`;
+const values = [`%${process.argv[2]}%`, process.argv[3]];
+pool.query(queryString, values)
 .then (res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`)
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = $1 || 'JUL02'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+const values = [`${process.argv[2]}`];
+
+pool.query(queryString, values)
 .then (res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`)
