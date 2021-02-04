cb7e2c5350c14381e275f417739f8db7def2a749
Sabia Tahir <sabiatahir@hotmail.com>
2021-02-04 02:17:05 +0000

    Updated to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -35,8 +35,8 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts on cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3]};
+WHERE cohorts.name LIKE $1
+LIMIT $2;
 `)
 .then (res => {
   res.rows.forEach(user => {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -14,7 +14,7 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1 || 'JUL02'
 ORDER BY teacher;
 `)
 .then (res => {
